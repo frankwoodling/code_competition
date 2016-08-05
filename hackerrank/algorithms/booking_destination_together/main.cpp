3f1cfca54d04af46c98b484c29c87c574c230cd0
Frank Woodling <fwoodling@gmail.com>
2016-08-05 18:23:37 -0400

    booking.com contest question

---
+++ hackerrank/algorithms/booking_destination_together/main.cpp
@@ -0,0 +1,32 @@
+// HackerRank Contest
+// Booking.com, Destination:Together <3
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n, m, c;
+
+    cin >> n >> m >> c;
+
+    int j_unique = n - c;
+    int z_unique = m - c;
+
+    int tot = j_unique + z_unique + c - 1;
+
+    int factorial = 1;
+
+    for(int i = 1; i <= tot; i++)
+    {
+        factorial=factorial*i;
+    }
+
+    cout << factorial;
+    return 0;
+}
--- hackerrank/algorithms/simple_array_sum/main.cpp
+++ hackerrank/algorithms/simple_array_sum/main.cpp
@@ -2,35 +2,27 @@
 // Algorithms, Simple Array Sum
 
 #include <cmath>
-#include <cstdio>
 #include <vector>
 #include <iostream>
-#include <algorithm>
-#include <array>
-using namespace std;
 
+using namespace std;
 
 int main()
 {
-    int arr[5] = {1, 2, 3, 6, 7};
-
-    cout << sizeof(arr)/sizeof(arr[0]) << endl;
-
-
-    for (int i = 0; i <= 5; i++)
-    {
-        cout << arr[i];
-    }
-
-}
-/*
-int main(){
     int n;
     cin >> n;
     vector<int> arr(n);
-    for(int arr_i = 0;arr_i < n;arr_i++){
+    for(int arr_i = 0;arr_i < n;arr_i++)
+    {
         cin >> arr[arr_i];
     }
+
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    cout << sum;
+
     return 0;
 }
- */
