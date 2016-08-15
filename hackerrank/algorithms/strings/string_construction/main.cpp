90ddd07eeef042bad777736aaa0b52115ae417d3
Frank Woodling <fwoodling@gmail.com>
2016-08-15 03:33:13 -0400

    move dir

--- hackerrank/algorithms/strings/string_construction/main.cpp
+++ hackerrank/algorithms/strings/string_construction/main.cpp
@@ -1,2 +1,13 @@
 // HackerRank Practice Problems
 // Algorithms > Strings > String Construction
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+}
+
+// use 2 for loops to find all substrings starting with the largest
+// then compare the substring to next portion of problem string
