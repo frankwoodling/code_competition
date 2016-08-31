935530f9b2789f53cac06047ef8b17948d5a4323
Frank Woodling <fwoodling@gmail.com>
2016-08-30 23:38:27 -0400

    add hackerrank algo problem

---
+++ hackerrank/algorithms/search/sherlock_and_array/main.cpp
@@ -0,0 +1,26 @@
+// HackerRank Practice Problem
+// Algorithms > Search > Sherlock and Array
+// incomplete
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int t, n, temp;
+    cin >> t;
+
+
+    for(int i = 0; i < t; i++){
+        cin >> n;
+        vector<int> arr{n};
+
+        for(int j = 0; j < n; j++){
+            arr[j] = temp;
+        }
+    }
+
+    for(int i = 0; i < arr.size(); i++)
+
+
+    return 0;
+}
