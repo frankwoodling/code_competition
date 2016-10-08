8a0f50cf3f2c8005928119b66e7d5419cb7ad9af
Frank Woodling <fwoodling@gmail.com>
2016-10-07 23:36:12 -0400

    hr bon appetite algo problem

--- hackerrank/algorithms/implementation/bon_appetit/main.cpp
+++ hackerrank/algorithms/implementation/bon_appetit/main.cpp
@@ -2,11 +2,28 @@
 // Algorithms > Implementation > Bon Appétit
 
 #include <iostream>
-using namespace std;
 
 int main() {
-    long long int n, k;
-    cin
+    long long int n, k, actual, temp, charged = 0;
+    std::cin >> n >> k;
+
+    for(int i = 0; i < n; i++) {
+        std::cin >> temp;
+        charged += temp;
+
+        if(i == k) {
+            charged -= temp;
+        }
+    }
+
+    std::cin >> actual;
+
+    if(charged/2 == actual) {
+        std::cout << "Bon Appetit";
+    }
+    else {
+       std::cout << abs(charged/2-actual);
+    }
 
     return 0;
 }
